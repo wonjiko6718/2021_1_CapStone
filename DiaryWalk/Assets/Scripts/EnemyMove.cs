adab70c6709be9d486f4b42569c90554e0328052
mingi0416 <mingi0416@gmail.com>
2021-04-26 15:01:01 +0900

    1
    1

---
+++ DiaryWalk/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    public int nextMove;
+    public GameObject Enemy;
+    public GameObject Player;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 3);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+    
+     }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            anim.SetTrigger("Screaming");
+            Destroy(gameObject,.60f);
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1 , 2);
+
+        anim.SetInteger("MoveSpeed", nextMove);
+        Invoke("Think", 3);
+        if(nextMove != 0)
+        spriteRenderer.flipX = nextMove == -1;
+    }
+
+}
---
+++ DiaryWalk/Assets/Scripts/Enemy_1Move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1Move : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    public int nextMove;
+    public GameObject Enemy_1;
+    public GameObject Player;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            anim.SetTrigger("");
+        
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        anim.SetInteger("MoveSpeed", nextMove);
+        Invoke("Think", 5);
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == -1;
+    }
+
+}
