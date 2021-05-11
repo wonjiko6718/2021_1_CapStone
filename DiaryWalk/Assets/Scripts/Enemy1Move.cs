3ed43b575336cd8ef723558d237d2580f26dc8f1
mingi0416 <mingi0416@gmail.com>
2021-05-11 17:19:27 +0900

    11

---
+++ DiaryWalk/Assets/Scripts/Enemy1Move.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Move : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    public int nextMove;
+    public GameObject Enemy;
+    public GameObject Player;
+    SpriteRenderer spriteRenderer;
+    Transform target;
+    float moveSpeed = 3f;
+    float contactDistance = 1f;
+    bool follow = false;
+    string dist = "";
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        if (Vector2.Distance(transform.position, target.position) > contactDistance && follow)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime * 0.4f);
+
+            float distance = Vector3.Distance(target.position, transform.position);
+
+
+    }
+
+ /*   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        if (follow)
+        {
+            if (target.position.x < transform.position.x)
+                dist = "Left";
+            else if (target.position.x > transform.position.x)
+                dist = "Right";
+        }
+        else
+        {
+            if (nextMove == -1)
+                dist = "Left";
+            else if (nextMove == 2)
+                dist = "Right";
+        }
+
+        if(dist == "Left")
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (dist == "Right")
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }*/
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetTrigger("Chasing");
+            follow = true;
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetTrigger("isWalking");
+            follow = false;
+            
+        }
+    }
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 3);
+        
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+    }
+
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+        anim.SetInteger("MoveSpeed", nextMove);
+        Invoke("Think", 3);
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == -1;
+    }
+
+}
---
+++ DiaryWalk/Assets/Scripts/qwe.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class qwe : MonoBehaviour
+{
+	public Transform target;
+	public Vector3 direction;
+	public float velocity;
+	public float accelaration;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		MoveToTarget();
+	}
+
+	public void MoveToTarget()
+	{
+		
+		target = GameObject.Find("Player").transform;
+
+		velocity = (velocity + Time.deltaTime);
+		
+		float distance = Vector3.Distance(target.position, transform.position);
+		
+		if (distance <= 8f)
+		{
+			this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+												   transform.position.y + (direction.y * velocity));
+		}
+		
+		else
+		{
+			velocity = 0.0f;
+		}
+	}
+}
